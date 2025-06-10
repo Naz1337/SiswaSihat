0dc0eb25c61666773756523bdc133b86967b1a46
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2025-06-11 01:46:50 +0800

    kek

--- app/src/main/java/com/mad/satu_c/group_satu/siswasihat/MainActivity.java
+++ app/src/main/java/com/mad/satu_c/group_satu/siswasihat/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mad.satu_c.group_satu.siswasihat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 5000; // 5 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the DashboardActivity. */
+                Intent mainIntent = new Intent(MainActivity.this, DashboardActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
