7792dc407559bf9eeb289c708e94f06c4884fb98
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2025-06-11 01:11:33 +0800

    cline!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -40,4 +44,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+    implementation("com.google.firebase:firebase-firestore")
 }
